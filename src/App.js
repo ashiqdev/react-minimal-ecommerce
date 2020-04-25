31dcb8bffe124b7278a0ce3df3497e92a7fe66f2
Ashiqur Rahman <ashiquejava@gmail.com>
2020-04-25 16:54:22 +0600

    feat: checkout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import NavBar from "./components/Navbar/Navbar";
+import ThemeContext from "./ThemeContext";
+
+import Checkout from "./components/CheckOut/CheckOut";
+import Home from "./components/Home/Home";
+import ProductDetails from "./components/Products/ProductDetails";
+import { StateProvider } from "./store";
+
+import "./App.css";
+import Cart from "./components/Cart/Cart";
 
 function App() {
+  const [dark, setDark] = useState(false);
+
+  const toggleTheme = () => setDark((isDark) => (isDark = !isDark));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StateProvider>
+      <ThemeContext.Provider value={{ dark, toggle: toggleTheme }}>
+        <div className={`App ${dark ? "dark" : "light"}`}>
+          <Router>
+            <NavBar />
+            <Switch>
+              <Route path="/checkout" component={Checkout} />
+
+              <Route path="/product/:productid" component={ProductDetails} />
+
+              <Route path="/" component={Home} />
+            </Switch>
+            <Cart />
+          </Router>
+        </div>
+      </ThemeContext.Provider>
+    </StateProvider>
   );
 }
 
